06700bc0750e81a620cb04e2fa6267632cbb52af
vaibhavwaghode <waghode.vaibhav@gmail.com>
2024-11-14 00:51:54 +0530

    crud operation in progress

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,72 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const FriendModel = require('./models/friend');
+
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect(
     "mongodb://localhost:27017/mern-todo",
     {useNewUrlParser:true}
 );
 
-app.get('/insert',async (req,res)=>{
-    const friend = new FriendModel({
-        name: "bhalaldevaaa",
-        age: '29',
-        discription: "cheater charismatic warrior"
-    })
-    await friend.save();
-    res.send("Inserted Data")
-})
+app.post('/addfriend',async (req,res)=>{
+    const {name,age} = req.body;
 
+    try{
+        const friend = new FriendModel({
+            name: name,
+            age: age
+        })
+        await friend.save();
+        res.send("Inserted Data")
+    }catch(error){
+        console.log("internal server error",error.message)
+    }
+
+})
 
 app.get('/read',async (req,res)=>{
 
-    try{
-        const result = await FriendModel.find();
-        res.status(200).json(result);
-    }catch(err){
-        res.status(500).json(err);
-    }
+        try{
+            const result = await FriendModel.find();
+            res.status(200).json(result);
+        }catch(err){
+            res.status(500).json(err);
+        }
 
-    // FriendModel.find()
-    //     .then((data)=>{
-    //             res.status(200).json(data)
-    //     })
-    //     .catch((error)=>{
-    //         res.status(500).json(error)
-    //     })
 })
+
+// app.get('/insert',async (req,res)=>{
+//     const friend = new FriendModel({
+//         name: "bhalaldevaaa",
+//         age: '29',
+//         discription: "cheater charismatic warrior"
+//     })
+//     await friend.save();
+//     res.send("Inserted Data")
+// })
+
+
+// app.get('/read',async (req,res)=>{
+
+//     try{
+//         const result = await FriendModel.find();
+//         res.status(200).json(result);
+//     }catch(err){
+//         res.status(500).json(err);
+//     }
+
+//     // FriendModel.find()
+//     //     .then((data)=>{
+//     //             res.status(200).json(data)
+//     //     })
+//     //     .catch((error)=>{
+//     //         res.status(500).json(error)
+//     //     })
+// })
 app.listen( 3030 ,()=>{
     console.log("server is running on port 3030")
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect} from 'react';
+import Axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ name ,setName ] = useState("");
+  const [ age, setAge ] = useState(0);
+  const [listOfFriend,SetListOfFriend] = useState([])
+
+  const handleName = (event) =>{
+    setName(event.target.value);
+  }
+
+  const handleAge = (event) => {
+      setAge(event.target.value);
+  }
+
+  const AddFriend = () =>{
+
+    Axios.post('http://localhost:3030/addfriend',{
+      name: name,
+      age: age
+    })
+    .then((data)=>{
+      SetListOfFriend([...listOfFriend,{name:name,age:age}])
+      console.log("friend added",data)
+    })
+    .catch((error)=>{
+      console.log("fail to add friend",error)
+    })
+  }
+
+
+  useEffect(()=>{
+    const fetchData = () =>{
+      Axios.get('http://localhost:3030/read')
+        .then((response)=>{
+          SetListOfFriend(response.data);
+          console.log(response);
+        })
+        .catch((error)=>{
+          console.log(error)
+        })
+
+    }
+    fetchData()
+  },[])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='input-box'>
+
+        <div className="inputs">
+          <input type="text" placeholder='enter name here..' onChange={handleName}/>
+          <input type="number" placeholder='enter age here..' onChange={handleAge} />
+
+          <button className='add-btn' onClick={AddFriend}>Add Friend</button>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div  className='Listoffriends'>
+        {listOfFriend.map((data,key)=>{
+              return <div  className='friendContainer'>
+                            <div  className='friend' key={key}>
+                                <h3>{data.name}</h3>
+                                <h3>{data.age}</h3>
+                            </div>
+                            <button>Update</button>
+                            <button id="remove-btn-border">X</button>
+                      </div>
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
     </>
   )
 }
